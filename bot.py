2af5ce8cc619c0481c2f03ccbec53f7d9675aa3b
nobodyrightnow <192860211+nobodyrightnow@users.noreply.github.com>
2026-07-26 17:11:35 -0500

    Refactor bot.py to load Discord token from .env file and update .gitignore

--- bot.py
+++ bot.py
@@ -1,4 +1,14 @@
 import discord
+from dotenv import load_dotenv # for loading discord token from .env file
+import os
+
+# Load discord token
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+# Check to see if discord token could be loaded
+if DISCORD_TOKEN is None:
+    raise ValueError("DISCORD_TOKEN is missing from .env")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,8 +23,8 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
+
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('MTUxMjUxNTc1Mjg2NTk1NTkzMA.GS5LDB.j8XhMcZxLKQvTV6FR_iYsq01pC24PFNKsK4hC8')
+client.run(DISCORD_TOKEN)
