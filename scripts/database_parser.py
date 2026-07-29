1528d09bf78d63709f96fa8918b11579e4cc9757
nobodyrightnow <192860211+nobodyrightnow@users.noreply.github.com>
2026-07-29 14:05:57 -0500

    Remove class generation logic from database parser

--- scripts/database_parser.py
+++ scripts/database_parser.py
@@ -15,31 +15,10 @@ from config import (
     OMITTED_ORDER, 
     PROCESSED_DATA,
     CLASS,
-    CLASS_DATA,
     PROCESSED_SPECIES_PROFILE,
     PROFILE_COLS,
     EXTINCT
 )
-
-print("Generating broader files to help the model narrow down...")
-
-# Generate a classes file for the model to use
-output = []
-
-for name in CLASS:
-    output.append(
-        {
-            "class_name": name,
-            "prompts": [
-                f"a species from {name}"
-            ]
-        }
-    )
-
-with open(CLASS_DATA, "w", encoding="utf-8") as f:
-    json.dump(output, f, indent=4, ensure_ascii=False)
-
-print("Finished!")
 
 print("Starting to read common names.")
 
