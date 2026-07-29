4601b9f2153e0a5205320f2e6c09ff49969a2489
nobodyrightnow <192860211+nobodyrightnow@users.noreply.github.com>
2026-07-29 00:13:22 -0500

    Removed mention of total_names

--- scripts/database_parser.py
+++ scripts/database_parser.py
@@ -25,7 +25,7 @@ common_names = vernacular[
 ]
 
 new_num = len(vernacular)
-print(f"Filtered out {total_names - new_num} of different languages. There are now {new_num} names")
+print(f"Filtered out {total - new_num} of different languages. There are now {new_num} names")
 total = new_num
 
 """
