8173f80990ccec971a5cf80599950f4c7d93ad1e
nobodyrightnow <192860211+nobodyrightnow@users.noreply.github.com>
2026-08-04 00:52:13 -0500

    Refactor message handling to respond to image attachments; remove unused command

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,6 +1,7 @@
 import discord
 from dotenv import load_dotenv # for loading discord token from .env file
 import os
+from image_analyzer import analyze_image
 
 # Load discord token
 load_dotenv()
@@ -21,10 +22,30 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # ignore bot's own messages
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # check if the bot was mentioned
+    if client.user in message.mentions:
+        # check for image attachment
+        image_attachments = []
+        for attachment in message.attachments:
+            if attachment.content_type and attachment.content_type.startswith("image/"):
+                image_attachments.append(attachment)
+
+        num_images = len(image_attachments)
+        if num_images == 0:
+            await message.reply("Don't ping me without an image >:(")
+            return
+        elif num_images > 1:
+            await message.reply("Slow down! I can only handle one image at a time right now :(")
+            return
+
+        # download the image
+        image = await image_attachments[0].read()
+
+        await message.reply(analyze_image(image))
+        return
 
 client.run(DISCORD_TOKEN)
