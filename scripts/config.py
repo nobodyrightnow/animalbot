f340abd9e6b3e701a13bbe400e109cf688ed23bd
nobodyrightnow <192860211+nobodyrightnow@users.noreply.github.com>
2026-08-04 00:51:43 -0500

    Add detailed configs for omitting certain taxa, laying the groundwork for an improved system for embedding generation later on

--- scripts/config.py
+++ scripts/config.py
@@ -17,6 +17,65 @@ TAXA_EMBEDDINGS = CACHE / "taxa_embeddings.pt"
 
 RANKS = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
 LANGUAGE = {"eng"}
+STATUS = {"accepted", "provisionally accepted"}
+
+OMITTED_PHYLUM = {
+    # animalia
+    "Bryozoa", # tiny colonial aquatic invertebrates
+    "Entoprocta", # tiny sessile aquatic invertebrates
+    "Cycliophora", # microscopic commensal organisms found on lobsters' mouthparts
+    "Dicyemida", # kidney parasites of cephalopods
+    "Gastrotricha", # simple microscopic aquatic organisms
+    "Gnathostomulida", # nearly microscopic marine animals similar to flatworms
+    "Kinorhyncha", # tiny marine invertebrates that live in mud and sand
+    "Loricifera", # tiny marine armored animals that attach themselves to sediment
+    "Micrognathozoa", # microscopic freshwater animals
+    "Orthonectida", # microscopic parasites of marine invertebrates
+    "Placozoa", # extremely simple microscopic organisms
+    "Rotifera", # nearly microscopic zooplankton
+    "Tardigrada", # microscopic relatives of arthropods
+    "Xenacoelomorpha", # small marine flatworm-like animals
+}
+
+OMITTED_CLASS = {
+    # arthropoda
+    "Mystacocarida", # tiny marine crustaceans
+    "Copepoda", # near microscopic crutstaceans found basically everywhere there's water
+    "Tantulocarida", # microscopic parasitic crustaceans
+    "Cephalocarida", # "horseshoe shrimp" -- near microscopic crustaceans
+    "Protura", # tiny soil-dwelling hexapods
+    # platyhelminthes
+    "Monogenea", # parasitic flatworms found on the skin and gills of fish -- mostly microscopic
+    "Trematoda", # flukes -- small parasites that you do NOT want to see
+    "Cestoda", # tapeworms (yuck)
+}
+
+OMITTED_ORDER = {
+    # arachnida
+    "Sarcoptiformes", # microscopic mites
+}
+
+OMITTED_FAMILY = {
+    # trombidiformes
+    "Demodecidae", # microscopic mammalian mites
+    "Tanaupodidae", # there is zero information on these guys
+    "Allotanaupodidae", # also zero information
+    "Tarsonemidae", # microscopic thread-footed mites
+    "Tydeidae", # nearly microscopic soft-bodied mites
+    "Veigaiidae", # microscopic free-living mites
+}
+
+COLLAPSE_TO_PHYLUM = {
+    # animalia
+    "Nematomorpha", # horsehair worms
+    "Chaetognatha", # arrow worms
+    "Priapulida", # penis worms
+}
+
+COLLAPSE_TO_CLASS = {
+
+}
+
 
 a_an = inflect.engine()
 
